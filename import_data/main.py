82be63db10cdb15d4136bbeb218966c2d738dc4a
Borislav Yanev <bobkatabo@protonmail.com>
2023-01-11 15:30:16 +0200

    successfull import script yeeeeee

---
+++ import_data/main.py
@@ -0,0 +1,94 @@
+import asyncio
+import glob
+import json
+import ast
+from pprint import pprint
+import re
+import requests
+import urllib.request
+
+headers = {'content-type': 'application/json'}
+
+folder_path = '../extract_data/articles-biopedia/*.js'
+
+def prepare_article(data):
+    def get(value):
+        return data.get(value)
+    article = {
+        'title': get('title'),
+        'description': get('description'),
+        'slug': get('slug'),
+        'content': get('content'),
+        'metaTitle': get('metaTitle') or '',
+        'releaseDate': get('releaseDate'),
+        'readTime': get('readTime') or '5 мин.',
+        'popular': get('popular') == 'True',
+        'exclusive': get('exclusive') == 'True',
+        'exclusiveText': get('exclusiveText') or '',
+        'editorChoice': get('editorChoice') == 'True',
+        # 'image': get('image'),
+        'categories': [],
+        'site': {
+            'id': get('site').get('data').get('id')
+        },
+        'contentsTable': get('contentsTable')
+    }
+
+    for category in get('categories').get('data'):
+        article['categories'].append(category.get('id'))
+
+
+    return article
+
+def download_image(image):
+    image_url = image.get('url')
+    img_data = requests.get(image_url).content
+    files = {'files': (image.get('name'), img_data, 'image', {'uri': ''})}
+    response = requests.post('http://localhost:1337/api/upload', files=files)
+    json = response.json()
+    image_id = json[0].get('id')
+    print(json[0].get('id'))
+
+    return image_id
+
+async def prepare_image(image):
+    download_response = urllib.request.urlretrieve(image.get('url'), image.get('name'))
+    print('mAGAGAGAGAGAGAmm?')
+    files = {'files': ('Screenshot_5.png', open(image.get('name'), 'rb'), 'image', {'uri': ''})}
+    upload_response = requests.post('http://localhost:1337/api/upload', files=files, headers=headers)
+    print(upload_response.reason)
+
+    return await upload_response
+
+async def main():
+    print('bo')
+    index = 0
+    fails = 0
+    for file_path in glob.glob(folder_path):
+        with open(file_path) as f:
+            try:
+                file_object =  eval((f.read().replace('const data = ', '')))
+                article = prepare_article(file_object.get('attributes'))
+                # if (index < 3):
+                print(article.get('readTime'))
+                # for key, value in article:
+                #     print(key)
+                print('mmmkaay')
+                # print(file_object.get('attributes'))
+                try:
+                    image = file_object.get('attributes').get('image').get('data').get('attributes')
+                    image_id = download_image(image)
+                    print(image_id)
+                    article['image'] = image_id
+                    response = requests.post('http://127.0.0.1:1337/api/articles', json={"data": article})
+                    response.raise_for_status()
+                except requests.exceptions.RequestException as e:
+                    print("Request failed with error: ", e)
+                # print(image_response)
+            except:
+                fails += 1
+
+        index += 1
+
+
+asyncio.run(main())
